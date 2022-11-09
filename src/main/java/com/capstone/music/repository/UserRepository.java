7a33e1454cc6b09bca2cc6ef57e8d86508d0cb5f
kyj <khkkgon0118@gmail.com>
2022-11-09 21:00:24 +0900

    feat : RDS 연결 완료

--- src/main/java/com/capstone/music/repository/UserRepository.java
+++ src/main/java/com/capstone/music/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.capstone.music.repository;
 
 import com.capstone.music.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
-    User findById(Long id);
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/capstone/music/service/HomeService.java
+++ src/main/java/com/capstone/music/service/HomeService.java
@@ -6,6 +6,8 @@ import com.capstone.music.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class HomeService {
@@ -13,7 +15,7 @@ public class HomeService {
     private final UserRepository userRepository;
 
     public GetHomeRes home(Long user_id) {
-        User user = userRepository.findById(user_id);
+        Optional<User> user = userRepository.findById(user_id);
         GetHomeRes getHomeRes = GetHomeRes.builder().build();
         return getHomeRes;
     }
