da6935bc66bd9e726c2ab35dd2433818e7f290bf
myeondii <khkkgon0118@gmail.com>
2022-11-27 02:42:15 +0900

    feat : 음악 test api

--- src/main/java/com/capstone/music/controller/TestController.java
+++ src/main/java/com/capstone/music/controller/TestController.java
@@ -1,8 +1,25 @@
 package com.capstone.music.controller;
 
+import com.capstone.music.dto.JoinReqDTO;
+import com.capstone.music.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
 public class TestController {
 
-    // 테스트 시작
+    private final TestService testService;
 
-    // 테스트 결과 (저장)
+    // 테스트 시작 + 결과 응답
+    @PostMapping("/test/user/{user_id}")
+    public ResponseEntity<> join(@PathVariable Long user_id) throws IOException {
+
+        return new ResponseEntity<>(, HttpStatus.OK);
+    }
 }
---
+++ src/main/java/com/capstone/music/service/TestService.java
@@ -0,0 +1,9 @@
+package com.capstone.music.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TestService {
+}
