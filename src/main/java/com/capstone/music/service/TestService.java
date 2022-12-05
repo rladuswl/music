07a1b4b0903630a6ec570fa08c42423c455b6da3
myeondii <khkkgon0118@gmail.com>
2022-12-06 00:23:18 +0900

    feat : music test api

--- src/main/java/com/capstone/music/service/TestService.java
+++ src/main/java/com/capstone/music/service/TestService.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Service;
 public class TestService {
 
     public String test(Long user_id, PostTestAnswersDTO postTestAnswersDTO) {
+        // 대답에 따라 경우의 수 만들기 -> 결과 결정하기
+
         String result = null;
+
         return result;
     }
 }
