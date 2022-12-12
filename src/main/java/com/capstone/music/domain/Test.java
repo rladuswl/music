137754f40159128c6a75667a3dcbc3fbccc5413c
myeondii <khkkgon0118@gmail.com>
2022-12-12 20:18:41 +0900

    Delete Test.java

--- src/main/java/com/capstone/music/domain/Test.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.music.domain;
-
-public class Test {
-    private String question1;
-    private String question2;
-    private String question3;
-    private String question4;
-    private String question5;
-}
