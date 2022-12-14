65ed9b2526d255d82bf0b41fbc20c85f6065d186
myeondii <khkkgon0118@gmail.com>
2022-12-14 19:02:02 +0900

    Delete Test_result.java

--- src/main/java/com/capstone/music/domain/Test_result.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.music.domain;
-
-public class Test_result {
-    private String result1;
-    private String result2;
-    private String result3;
-    private String result4;
-    private String result5;
-}
